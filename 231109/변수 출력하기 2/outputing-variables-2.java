b2983cbf0627783c7ca0f9a678cdf16a2f0fbbd0
gusdn0108 <96761523+gusdn0108@users.noreply.github.com>
2023-11-09 12:46:25 +0900

    Title: 변수 출력하기 2, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231109/변수 출력하기 2/outputing-variables-2.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        String b = "C";
+        String comma = "!.....!";
+        System.out.println(a + comma+b);
+    }
+}
