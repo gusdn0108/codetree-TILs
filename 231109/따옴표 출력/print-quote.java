aacadc3c5d1d65a488b1fcb23719c4b0cdc6d13d
gusdn0108 <96761523+gusdn0108@users.noreply.github.com>
2023-11-09 12:36:12 +0900

    Title: 따옴표 출력, Time: 82ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 231109/따옴표 출력/print-quote.java
+++ 231109/따옴표 출력/print-quote.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        String a = "He says";
-        String b = "\"It's a really simple sentence\".";
-        System.out.println(a+b);
+        System.out.println("He says"+"\"It's a really simple sentence\".");
     }
 }
