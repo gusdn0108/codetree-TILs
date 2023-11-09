d33956a7642d1216add816390b3cc74df3595abf
gusdn0108 <96761523+gusdn0108@users.noreply.github.com>
2023-11-09 13:14:59 +0900

    Title: 합을 복사하기, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231109/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        int temp = a+b+c;
+        String blank = " ";
+
+        a = temp;
+        b = temp;
+        c = temp;
+        System.out.println(a+blank+b+blank+c);
+    }
+}
