44f5f460f8f7886005395e306fbb84504c9db8b8
gusdn0108 <96761523+gusdn0108@users.noreply.github.com>
2023-11-09 13:46:54 +0900

    Title: 1시간 뒤 시간 출력, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 231109/1시간 뒤 시간 출력/print-one-hour-later.java
+++ 231109/1시간 뒤 시간 출력/print-one-hour-later.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        sc.useDelimiter(":");
+        int h = sc.nextInt();
+        int m = sc.nextInt();
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
-        System.out.println(a+1+":"+b);
+        System.out.println((h+1)+":"+m);
     }
 }
