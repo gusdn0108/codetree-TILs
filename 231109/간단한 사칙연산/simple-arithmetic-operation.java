976dfcec5c346992f710887a0ef7d7d21a4f00bc
gusdn0108 <96761523+gusdn0108@users.noreply.github.com>
2023-11-09 14:10:24 +0900

    Title: 간단한 사칙연산, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 231109/간단한 사칙연산/simple-arithmetic-operation.java
+++ 231109/간단한 사칙연산/simple-arithmetic-operation.java
@@ -9,7 +9,8 @@ public class Main {
         System.out.println(a+b);
         System.out.println(a - b);
         System.out.println(a/b);
-        System.out.println(Math.floorMod(a,b));
+//        System.out.println(Math.floorMod(a,b));
+        System.out.println(a % b);
 
     }
 }
