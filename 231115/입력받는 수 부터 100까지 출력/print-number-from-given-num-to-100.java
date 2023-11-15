58e6598d5bfecb1096a9b1c25b126f4e70548fac
gusdn0108 <96761523+gusdn0108@users.noreply.github.com>
2023-11-15 22:30:31 +0900

    Title: 입력받는 수 부터 100까지 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 231115/입력받는 수 부터 100까지 출력/print-number-from-given-num-to-100.java
+++ 231115/입력받는 수 부터 100까지 출력/print-number-from-given-num-to-100.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-         Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
         for (int i = number; i <= 100 ; i++) {
             System.out.println(i+" ");
