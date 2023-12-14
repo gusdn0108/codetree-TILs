4671cd5033666f632846fad430e7386c059d602a
gusdn0108 <96761523+gusdn0108@users.noreply.github.com>
2023-12-14 10:29:37 +0900

    Title: 정사각형 두 개 출력, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 231214/정사각형 두 개 출력/output-two-rectangles.java
+++ 231214/정사각형 두 개 출력/output-two-rectangles.java
@@ -7,7 +7,7 @@ public class Main {
         
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                System.out.print("*")''
+                System.out.print("*");
             }
             System.out.println();
         }
@@ -15,7 +15,7 @@ public class Main {
 
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                System.out.print("*")''
+                System.out.print("*");
             }
             System.out.println();
         }
