89c63736b6ef64077576246f08a17fcdb5ddea4d
gusdn0108 <96761523+gusdn0108@users.noreply.github.com>
2023-12-17 11:38:15 +0900

    Title: 건너뛰고 별표 출력하기, Time: 168ms, Memory: 10MB, Status: Passed - CodeTree

--- 231217/건너뛰고 별표 출력하기/skip-and-print-a-star.java
+++ 231217/건너뛰고 별표 출력하기/skip-and-print-a-star.java
@@ -6,18 +6,18 @@ public class Main {
         int n = sc.nextInt();
 
         for (int i = 0; i < n; i++) {
-            System.out.println();
             for (int j = 0; j < i+1; j++) {
                 System.out.print("*");
             }
+            System.out.println();
             System.out.println();
         }
 
         for (int i = n-2; i > -1; i--) {
-            System.out.println();
             for (int j = 0; j < i+1; j++) {
                 System.out.print("*");
             }
+            System.out.println();
             System.out.println();
         }
     }
