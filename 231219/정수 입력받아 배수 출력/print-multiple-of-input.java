785d7ea2fda797ac724234ec274424174e9d4abc
gusdn0108 <96761523+gusdn0108@users.noreply.github.com>
2023-12-19 17:42:09 +0900

    Title: 정수 입력받아 배수 출력, Time: 168ms, Memory: 10MB, Status: Passed - CodeTree

--- 231219/정수 입력받아 배수 출력/print-multiple-of-input.java
+++ 231219/정수 입력받아 배수 출력/print-multiple-of-input.java
@@ -3,14 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        // 변수 선언
-        int n;
-
-        // 입력
-        n = sc.nextInt();
+        int n = sc.nextInt();
         
-        // 출력
         for(int i = n; i <= 5 * n; i += n)
             System.out.print(i + " ");
     }
