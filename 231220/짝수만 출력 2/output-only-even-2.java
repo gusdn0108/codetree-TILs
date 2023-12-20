2bb2db9bdb2e822f9c454ec7604ae59d68fcb5bd
gusdn0108 <96761523+gusdn0108@users.noreply.github.com>
2023-12-20 12:51:22 +0900

    Title: 짝수만 출력 2, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

--- 231220/짝수만 출력 2/output-only-even-2.java
+++ 231220/짝수만 출력 2/output-only-even-2.java
@@ -2,13 +2,13 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        int n = sc.nextInt(); // 20
+        int m = sc.nextInt();   // 10
 
         while (n >= m){
-            if(n % 2 == 1 ){
+            if(n % 2 == 0 ){
                 System.out.print(n+" ");
-                n--;
+                n -=2;
             }
         }
     }
