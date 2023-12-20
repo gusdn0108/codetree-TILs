51b01d8a26c3f3d85c9ebde82622f88fdfc683e6
gusdn0108 <96761523+gusdn0108@users.noreply.github.com>
2023-12-20 12:53:11 +0900

    Title: 문자 출력하기, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231220/문자 출력하기/print-text.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String n = sc.nextLine();
+        for (int i = 0; i < 8; i++) {
+            System.out.print(n);
+        }
+    }
+}
