83e43b9ff272c0d4f47b759bccf748e587b0cdfd
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-01-19 21:01:06 +0900

    Title: 배열의 특정 위치 찾기 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240119/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.java
+++ 240119/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.java
@@ -5,10 +5,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] arr = new int[9];
+        int[] arr = new int[100];
         int sum =0;
 
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
 
