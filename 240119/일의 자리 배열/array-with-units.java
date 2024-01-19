3fe8a0d7c7e1aa08f0404a7c15009e3ce92ee25c
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-01-19 22:27:58 +0900

    Title: 일의 자리 배열, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

--- 240119/일의 자리 배열/array-with-units.java
+++ 240119/일의 자리 배열/array-with-units.java
@@ -8,14 +8,14 @@ public class Main {
         int b = sc.nextInt();
 
         int[] arr = new int[10];
-        arr[1] = a;
-        arr[2] = b;
+        arr[0] = a;
+        arr[1] = b;
 
-        for (int i = 3; i < arr.length; i++) {
+        for (int i = 2; i < 10; i++) {
             arr[i] = (arr[i-2]+arr[i-1])%10;
         }
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.print(arr[i]+" ");
         }
 //        arr[1] =
