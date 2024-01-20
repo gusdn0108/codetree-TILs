7ff95c2e2a290e23cde23e77010d3c6f502fab30
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-01-20 15:54:14 +0900

    Title: 주사위 놀이, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240120/주사위 놀이/play-with-dice.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        int[] countArr = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+            countArr[arr[i]]++;
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+        for (int i = 1; i <= 6; i++) {
+            System.out.println(i+ " - "+ countArr[arr[i]]);
+        }
+
+
+
+    }
+}
