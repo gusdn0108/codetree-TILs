bbd9a8da27137e339ad0c9060c20e8214d40f786
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-01-22 02:30:29 +0900

    Title: 배열 놀이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240122/배열 놀이/play-with-array.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 원소 갯수
+        int n = sc.nextInt();
+        // q개의 질문
+        int q = sc.nextInt();
+        int[] arr = new int[100];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < q; i++) {
+            int qType = sc.nextInt();
+
+            if(qType == 1){
+                int a = sc.nextInt();
+                System.out.println(arr[a-1]);
+            }
+
+            else if(qType == 2){
+                int a = sc.nextInt();
+                int idx = -1;
+                for (int j = 0; j < n; j++) {
+                    if(arr[j] == a){
+                        idx =j;
+                        break;
+                    }
+                }
+                System.out.println(idx+1);
+            }
+
+
+            else {
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+
+                for (int j = a-1; j < b; j++) {
+                    System.out.print(arr[j]+" ");
+                }
+                System.out.println();
+            }
+        }
+
+    }
+
+
+}
