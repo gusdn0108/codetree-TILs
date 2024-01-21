0746c27e139e3162174548ac16e935f1e5f55632
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-01-22 03:11:43 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240122/2가 3번째로 등장하는 위치/where-2-appears-3rd.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n  = sc.nextInt();
+        int[] arr= new int[n];
+        int cnt = 0;
+        int idx = 0;
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            if(arr[i] == 2){
+                cnt++;
+                if(cnt == 3){
+                    idx = i;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(idx+1);
+
+
+    }
+}
