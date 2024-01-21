94b842c21ea201378f3df980fe8ffecd813c2740
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-01-22 03:02:03 +0900

    Title: 연속부분수열일까, Time: 169ms, Memory: 10MB, Status: Passed - CodeTree

--- 240122/연속부분수열일까/contiguous-array-or-not.java
+++ 240122/연속부분수열일까/contiguous-array-or-not.java
@@ -4,29 +4,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int[] arr = new int[a];
-        int[] arr2 = new int[b];
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int[] arr = new int[100];
+        int[] arr2 = new int[100];
 
-        for (int i = 0; i < a; i++) {
+        for (int i = 0; i < n1; i++) {
             arr[i] = sc.nextInt();
         }
 
-        for (int i = 0; i < b; i++) {
+        for (int i = 0; i < n2; i++) {
             arr2[i] = sc.nextInt();
         }
 
-        for (int i = 0; i < a; i++) {
+        for (int i = 0; i < n1; i++) {
             boolean success = true;
-            for (int j = 0; j < b; j++) {
-                if(i+j >= b){
+            for (int j = 0; j < n2; j++) {
+                if(i+j >= n1){
                     success= false;
                     break;
                 }
 
-                if(arr[i+j]!= arr2[j]){
-                    success= false;
+                if(arr[i + j] != arr2[j]) {
+                    success = false;
                     break;
                 }
             }
