368256355e00091185390ef11121732d062e9eaa
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-01-22 21:15:18 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240122/중복되지 않는 숫자 중 최대/max-of-unique-number.java
+++ 240122/중복되지 않는 숫자 중 최대/max-of-unique-number.java
@@ -21,7 +21,7 @@ public class Main {
         }
 
         for (int i = 0; i < n; i++) {
-            if(max == arr[i] ){
+            if(arr[i] == arr[i] ){
                 cnt++;
             }
         }
