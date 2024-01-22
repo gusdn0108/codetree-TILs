d43eb0547bc857c1b0a1e4172d213cba44a17abf
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-01-22 20:25:51 +0900

    Title: 999 또는 -999, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240122/999 또는 -999/999-or-999.java
+++ 240122/999 또는 -999/999-or-999.java
@@ -4,33 +4,34 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr =new int[100];
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
+        int[] arr = new int[100];
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 100; i++) {
             arr[i] = sc.nextInt();
-            if(arr[i] == 999 || arr[i] == -999){
-                arr[i] = arr[i-1];
+            if (arr[i] == 999 || arr[i] == -999) {
                 break;
             }
         }
 
+        int maxVal = arr[0];
+        int minVal = arr[0];
 
-        for (int i = 0; i < arr.length; i++) {
-            if(min > arr[i]){
-                min = arr[i];
+
+        for (int i = 1; i < 100; i++) {
+            if(arr[i] == 999|| arr[i] == -999){
+                break;
             }
+
+            if(arr[i]> maxVal){
+                maxVal = arr[i];
+            }
+            if(arr[i] < minVal){
+                minVal = arr[i];
+            }
+
         }
 
-        for (int i = 0; i < arr.length; i++) {
-                if(max < arr[i]){
-                    max = arr[i];
-                }
-        }
-
-
-        System.out.print(max+" "+min);
+        System.out.print(maxVal + " " + minVal);
 
 
     }
