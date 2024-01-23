3bff763584939ec7124d52ad4a373a5e0d95ba9a
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-01-23 22:35:56 +0900

    Title: 500 근처의 수, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 240123/500 근처의 수/near-500.java
+++ 240123/500 근처의 수/near-500.java
@@ -1,49 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
-    public static final int MAX_N = 1000;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int[] arr = new int[10];
 
-
-        int[] arr =  new int[10];
-
-        for (int i = 0; i < 10; i++) {
+        for(int i = 0; i < 10; i++)
             arr[i] = sc.nextInt();
+        
+
+        int maxVal = 1;
+        int minVal = 1000;
+
+        for(int i = 0; i < 10; i++) {
+            if(arr[i] < 500 && arr[i] > maxVal)
+                maxVal = arr[i];
+            
+            if(arr[i] > 500 && arr[i] < minVal)
+                minVal = arr[i];
         }
-
-
-        int min = arr[0];
-        int max = arr[0];
-        int maxAfter = arr[0];
-
-        for (int i = 0; i < 10; i++) {
-            if( 500 > arr[i]){
-                if(min <  arr[i]){
-                    min = arr[i];
-                }
-            }
-        }
-
-        for (int i = 0; i < 10; i++) {
-            if(min < 500 && min < arr[i]){
-                max = arr[i];
-            }
-
-            if( 500 < max && min < max){
-                maxAfter = arr[i];
-                break;
-            }
-        }
-
-        System.out.print(min+" "+maxAfter);
-
-
-
-
-
-
+        
+        System.out.print(maxVal + " " + minVal);
     }
 }
