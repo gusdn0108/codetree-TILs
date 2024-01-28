1dc14f2847549ed477bee403ae18bdeeef8771cf
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-01-28 17:22:58 +0900

    Title: 원소 10개의 합, Time: 173ms, Memory: 10MB, Status: Passed - CodeTree

--- 240128/원소 10개의 합/sum-of-10-elements.java
+++ 240128/원소 10개의 합/sum-of-10-elements.java
@@ -3,10 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
+        int[] arr = new int[10];
         int sum =0;
-        for (int i = 0; i <n ; i++) {
+        for (int i = 0; i <10 ; i++) {
             arr[i] = sc.nextInt();
             sum += arr[i];
         }
