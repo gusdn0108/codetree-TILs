ef00f908e3afa7c916c9b6a6fb1e160ee56f74ed
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-02-03 06:27:50 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

--- 240203/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.java
+++ 240203/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.java
@@ -8,8 +8,9 @@ public class Main {
 
         for (int i = 0; i < 100; i++) {
             arr[i] = sc.nextInt();
-            sum += arr[i];
-            if( arr[i] == 0){
+            if(arr[i] == 0){
+                sum += arr[i-1]+arr[i-2]+arr[i-3];
+
                 break;
             }
         }
