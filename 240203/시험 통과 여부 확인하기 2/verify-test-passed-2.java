79b90d221554584a23dd963fe0aabc84322e8298
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-02-03 05:18:55 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240203/시험 통과 여부 확인하기 2/verify-test-passed-2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int passPeople = 0;
+
+        for(int i = 0; i < n; i++) {
+
+            int[] arr = new int[4];
+            int sum = 0;
+
+            for(int j = 0; j < 4; j++) {
+                arr[j] = sc.nextInt();
+            }
+
+            for(int j = 0; j < 4; j++) {
+                sum += arr[j];
+            }
+
+            double avg = (double)sum / 4;
+
+            if(avg >= 60) {
+                System.out.println("pass");
+                passPeople++;
+            }
+            else
+                System.out.println("fail");
+        }
+
+        // 통과한 사람의 수 출력
+        System.out.println(passPeople);
+    }
+}
