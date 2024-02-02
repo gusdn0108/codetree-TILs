4243a04887850dce7368e4568da068c883896be7
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-02-03 05:42:46 +0900

    Title: 특정 위치의 숫자, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 240203/특정 위치의 숫자/numbers-in-specific-location.java
+++ 240203/특정 위치의 숫자/numbers-in-specific-location.java
@@ -3,13 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
         int[] arr = new int[10];
         int sum = 0;
 
         for (int i = 0; i < 10; i++) {
             arr[i] = sc.nextInt();
-            sum += arr[i];
+            if(i == 2){
+                sum += arr[i];
+            }
+
+            if(i == 4){
+                sum += arr[i];
+            }
+
+            if(i == 9){
+                sum += arr[i];
+            }
         }
 
         System.out.print(sum);
