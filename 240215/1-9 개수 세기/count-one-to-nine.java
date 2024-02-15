8f4273fd6c9d2a510341a26b2d1aa72e3e1aac49
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-02-15 16:39:45 +0900

    Title: 1-9 개수 세기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240215/1-9 개수 세기/count-one-to-nine.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int[] countArr = new int[9];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            countArr[arr[i]]++;
+        }
+
+        for (int i = 1; i < 9; i++) {
+            System.out.println(countArr[i]);
+        }
+
+
+    }
+}
