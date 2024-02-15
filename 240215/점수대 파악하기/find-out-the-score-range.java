cbf599071e726216e4f265eca427b144dddd9b67
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-02-15 20:52:37 +0900

    Title: 점수대 파악하기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240215/점수대 파악하기/find-out-the-score-range.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[100];
+        int[] countArr = new int[11];
+        int cnt = 0;
+        for (int i = 0; i < 100; i++) {
+            arr[i] = sc.nextInt();
+            cnt++;
+            if (arr[i] == 0) {
+                break;
+            }
+            countArr[arr[i] / 10]++;
+        }
+
+
+
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i*10 + " - "+ countArr[i]);
+        }
+
+    }
+}
