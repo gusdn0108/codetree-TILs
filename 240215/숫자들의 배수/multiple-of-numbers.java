9db837f90303f0360ec36dae6c8d7ee8553cc03b
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-02-15 16:05:57 +0900

    Title: 숫자들의 배수, Time: 190ms, Memory: 10MB, Status: Passed - CodeTree

--- 240215/숫자들의 배수/multiple-of-numbers.java
+++ 240215/숫자들의 배수/multiple-of-numbers.java
@@ -9,16 +9,16 @@ public class Main {
         arr[0] = sc.nextInt();
         for (int i = 1; i < arr.length; i++) {
             arr[i] = arr[i-1]+arr[0];
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(arr[i]+" ");
             if( arr[i] % 5 == 0){
                 cnt++;
             }
             if(cnt == 2){
                 break;
             }
-        }
-
-        for (int i = 0; i < 10; i++) {
-            System.out.print(arr[i]+" ");
         }
     }
 }
