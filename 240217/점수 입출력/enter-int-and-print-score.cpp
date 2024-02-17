d5117e490780116b6417563033c6fb730f604a17
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-02-17 23:59:52 +0900

    Title: 점수 입출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240217/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int score;
+    cin>>score;
+    cout<<"Your score is ";
+    cout<<score;
+    cout<<" point.";
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
