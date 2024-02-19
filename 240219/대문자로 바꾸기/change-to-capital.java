33dff6b2fe07bcb53f703e8059eab6ccf17bfb2a
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-02-19 19:44:36 +0900

    Title: 대문자로 바꾸기, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240219/대문자로 바꾸기/change-to-capital.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[][] arr2d = new char[5][3];
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
+                arr2d[i][j] = sc.next().charAt(0);
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
+                arr2d[i][j] += 'A' - 'a';
+                System.out.print(arr2d[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
