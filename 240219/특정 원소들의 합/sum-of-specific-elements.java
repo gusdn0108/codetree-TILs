cc5bb1751b9d9c0ec2d7aa7e0bb06373b338d404
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-02-19 21:17:24 +0900

    Title: 특정 원소들의 합, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240219/특정 원소들의 합/sum-of-specific-elements.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] arr2d = new int[4][4];
+
+        int sum =0;
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                arr2d[i][j] =sc.nextInt();
+            }
+        }
+
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <= i; j++) {
+                sum += arr2d[i][j];
+            }
+        }
+
+        System.out.println(sum);
+
+
+
+    }
+}
