8e58f84fc5c402783bae1c41ee0a611eb89fb990
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-02-19 20:43:56 +0900

    Title: 배열의 평균, Time: 145ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240219/배열의 평균/ave-of-array.java
+++ 240219/배열의 평균/ave-of-array.java
@@ -13,30 +13,37 @@ public class Main {
                 arr2d[i][j] = sc.nextInt();
             }
         }
-
+        // 가로
         for (int i = 0; i < 2; i++) {
-            sum =0;
+            sum = 0;
             for (int j = 0; j < 4; j++) {
                 sum += arr2d[i][j];
             }
             System.out.printf("%.1f ",(double) sum / 4);
+
         }
         System.out.println();
 
+        // 세로
         for (int j = 0; j < 4; j++) {
+            sum = 0;
             for (int i = 0; i < 2; i++) {
                 sum += arr2d[i][j];
-                System.out.printf("%.1f ",(double) sum /4);
             }
+            System.out.printf("%.1f ",(double) sum / 2);
+
         }
         System.out.println();
 
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j <4 ; j++) {
-               sum += arr2d[i][j];
-                System.out.printf("%.1f ",(double) sum / 8);
-            }
-        }
 
+        // 전체
+        sum = 0;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 4; j++) {
+                sum += arr2d[i][j];
+            }
+            System.out.printf("%.1f ",(double) sum / 8);
+
+        }
     }
 }
