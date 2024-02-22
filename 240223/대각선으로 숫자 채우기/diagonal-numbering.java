5f100ba78f5acbdf293ca9effcb7b3da7ed660dd
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-02-23 01:19:40 +0900

    Title: 대각선으로 숫자 채우기, Time: 230ms, Memory: 12MB, Status: Passed - CodeTree

--- 240223/대각선으로 숫자 채우기/diagonal-numbering.java
+++ 240223/대각선으로 숫자 채우기/diagonal-numbering.java
@@ -1,48 +1,53 @@
-import java.util.Arrays;
 import java.util.Scanner;
-import javax.swing.plaf.IconUIResource;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[][] arr2d = new int[100][100];
-        int num = 1;
 
-        for (int startCol = 0; startCol < m; startCol++) {
-            int currRow =0;
+       
+        int n, m;
+        n = sc.nextInt();
+        m = sc.nextInt();
+        int count = 1;
+
+      
+        int[][] arr = new int[100][100];
+
+        for(int startCol = 0; startCol < m; startCol++) {
+            int currRow = 0;
             int currCol = startCol;
 
-            while (currCol >= 0 && currRow < n){
-                arr2d[currRow][currCol]= num;
+            while(currCol >= 0 && currRow < n) {
+                arr[currRow][currCol] = count;
 
-                currRow++;
-                currCol++;
-                num++;
-            }
-        }
-
-        for (int startRow = 1; startRow < n; startRow++) {
-            int currRow = startRow;
-            int currCol = m -1;
-
-            while (currCol >= 0 && currRow < n){
-                arr2d[currRow][currCol] = num;
-
+              
                 currRow++;
                 currCol--;
-                num++;
+                count++;
             }
         }
 
-        for (int row = 0; row < n; row++) {
-            for (int col = 0; col < m; col++) {
-                System.out.print(arr2d[row][col]+" ");
+
+        for(int startRow = 1; startRow < n; startRow++) {
+            int currRow = startRow;
+            int currCol = m - 1;
+
+            while(currCol >= 0 && currRow < n) {
+                arr[currRow][currCol] = count;
+
+                
+                currRow++;
+                currCol--;
+                count++;
+            }
+        }
+
+
+        for(int row = 0; row < n; row++) {
+            for(int col = 0; col < m; col++) {
+                System.out.print(arr[row][col] + " ");
             }
             System.out.println();
         }
-
-
     }
 }
