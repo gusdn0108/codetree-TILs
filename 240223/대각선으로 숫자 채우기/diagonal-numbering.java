a762e0908a3772324b073138f9f2bb548caa0b1b
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-02-23 01:18:05 +0900

    Title: 대각선으로 숫자 채우기, Time: 117ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240223/대각선으로 숫자 채우기/diagonal-numbering.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import javax.swing.plaf.IconUIResource;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr2d = new int[100][100];
+        int num = 1;
+
+        for (int startCol = 0; startCol < m; startCol++) {
+            int currRow =0;
+            int currCol = startCol;
+
+            while (currCol >= 0 && currRow < n){
+                arr2d[currRow][currCol]= num;
+
+                currRow++;
+                currCol++;
+                num++;
+            }
+        }
+
+        for (int startRow = 0; startRow < n; startRow++) {
+            int currRow = startRow;
+            int currCol = m -1;
+
+            while (currCol >= 0 && currRow < n){
+                arr2d[currRow][currCol] = num;
+
+                currRow++;
+                currCol--;
+                num++;
+            }
+        }
+
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < m; col++) {
+                System.out.print(arr2d[row][col]+" ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
