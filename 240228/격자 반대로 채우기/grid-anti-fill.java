1d5db06eb82b25c6084a8dd507f19d77f6ab4cff
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-02-28 20:30:25 +0900

    Title: 격자 반대로 채우기, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

--- 240228/격자 반대로 채우기/grid-anti-fill.java
+++ 240228/격자 반대로 채우기/grid-anti-fill.java
@@ -3,51 +3,33 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n=0;
-        n = sc.nextInt();
-        int[][] arr = new int[n][n];
+        int n = sc.nextInt();
+
+        int[][] arr2d = new int[n][n];
 
         int count = 1;
 
-        if (n%2 == 0){
-            for(int c=n-1; c>=0; c--){
-            if (c%2 == 1){
-                for(int r=n-1; r>=0; r--){
-                    arr[r][c] = count;
+        for (int col = n-1; col >= 0 ; col--) {
+            if((n-1-col)% 2 ==0){
+                for (int row = n-1; row >= 0 ; row--) {
+                    arr2d[row][col] = count;
+                    count++;
+                }
+            }else {
+                for (int row = 0; row < n; row++) {
+                    arr2d[row][col] = count;
                     count++;
                 }
             }
-            else if (c%2 == 0){
-                for(int r=0; r<n; r++){
-                    arr[r][c] = count;
-                    count++;
-                }
-            }
-        }
         }
 
-        if (n%2 == 1){
-            for(int c=n-1; c>=0; c--){
-            if (c%2 == 0){
-                for(int r=n-1; r>=0; r--){
-                    arr[r][c] = count;
-                    count++;
-                }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr2d[i][j]+" ");
             }
-            else if (c%2 == 1){
-                for(int r=0; r<n; r++){
-                    arr[r][c] = count;
-                    count++;
-                }
-            }
-        }
-        }
-    
-        for(int i=0; i<n; i++) {
-	        for(int j=0; j<n; j++) {
-		        System.out.print(arr[i][j] +" "); 
-	        }
             System.out.println();
         }
+
+
     }
 }
