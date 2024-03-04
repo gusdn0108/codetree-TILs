ba547c5e88ef453f603c4dedf3d364c75eb65143
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-03-04 20:22:26 +0900

    Title: 문자열 두번 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240304/문자열 두번 출력하기/print-string-twice.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Scanner;
+R
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        System.out.println(str);
+        System.out.println(str);
+
+    }
+}
