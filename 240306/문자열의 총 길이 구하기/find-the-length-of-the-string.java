0391b09577d79797c03083fb3d95fea09195a5df
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-03-06 21:08:59 +0900

    Title: 문자열의 총 길이 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240306/문자열의 총 길이 구하기/find-the-length-of-the-string.java
+++ 240306/문자열의 총 길이 구하기/find-the-length-of-the-string.java
@@ -5,21 +5,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] arr =new String[n];
 
-        String[] arr= new String[10];
+        int lenAll = 0;
+        int count = 0;
 
-        int lenAll =0;
-
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.next();
         }
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < n; i++) {
             int len = arr[i].length();
             lenAll += len;
         }
 
-        System.out.println(lenAll);
+
+
+        for (int i = 0; i < n; i++) {
+            if(arr[i].charAt(0)=='a'){
+                count++;
+            }
+        }
+
+        System.out.print(lenAll+" "+count);
 
 
     }
