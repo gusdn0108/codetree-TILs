08295ef23c0893b124f0c266cbb5f81cbe9bcab6
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-03-09 03:40:47 +0900

    Title: 문자열에서 문자 출력, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240309/문자열에서 문자 출력/print-chars-from-word.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(str.charAt(i));
+        }
+
+
+    }
+}
