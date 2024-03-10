60d281794aa51c146f2fe061d5f65fc7dc1bd055
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-03-10 17:16:03 +0900

    Title: 문자열 추가하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240310/문자열 추가하기/add-spring.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String str = sc.next();
+
+        String str1 = "Hello";
+
+        System.out.print(str+str1)
+
+
+        
+
+    }
+}
