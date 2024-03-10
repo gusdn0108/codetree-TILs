50f4fe154d58f9f4dc229ff4fe12e52c6fe2b675
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-03-10 19:33:46 +0900

    Title: 특정 문자의 유무, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240310/특정 문자의 유무/specific-character-presence.java
+++ 240310/특정 문자의 유무/specific-character-presence.java
@@ -5,16 +5,27 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         boolean isExisted = false;
+        boolean isExisted1 = false;
         String strExisted = "ee";
         String strExisted1 = "ab";
 
         for (int i = 0; i < str.length()-1; i++) {
-            if(str.charAt(i) == strExisted.charAt(i) && str.charAt(i) == strExisted1.charAt(i)){
+            if(str.charAt(i) == strExisted.charAt(i)){
                 isExisted = true;
             }
+            if(str.charAt(i) == strExisted1.charAt(i)){
+                isExisted1 = true;
+            }
+
         }
 
         if(isExisted == true){
+            System.out.println("Yes");
+        }else {
+            System.out.println("No");
+        }
+
+        if(isExisted1 == true){
             System.out.println("Yes");
         }else {
             System.out.println("No");
