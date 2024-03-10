5c9fde6f29c941ebe58bc86d300053ad3eea3a86
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-03-10 21:25:58 +0900

    Title: 문자열의 특정 위치 찾기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240310/문자열의 특정 위치 찾기/find-specific-location-in-spring.java
@@ -0,0 +1,22 @@
+import java.utils.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String chr = sc.next();
+        
+        int location = 0;
+
+        for(int i = 0; i < str.length()-1; i++){
+            if(str.chartAt(i) == chr){
+                location = i;
+            }else{
+                System.out.print("No")
+            }
+        }
+        
+    }
+}
