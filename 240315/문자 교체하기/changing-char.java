3e10e0fe3832b939009f0c164614e5395dfe92ab
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-03-15 05:13:13 +0900

    Title: 문자 교체하기, Time: 109ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240315/문자 교체하기/changing-char.java
+++ 240315/문자 교체하기/changing-char.java
@@ -6,18 +6,8 @@ public class Main {
         String str = sc.next();
         String str1 = sc.next();
 
-        char[] charArray = str.toCharArray();
-        char[] charArray1 = str1.toCharArray();
-        char temp =' ';
-        char temp1 = ' ';
-        charArray[0] = temp;
-        charArray[1] = temp1;
+        str1 = str.substring(0,1)+str1.substring(2);
 
-
-        charArray1[0] = temp;
-        charArray1[1] = temp1;
-
-        String s = String.valueOf(charArray1);
-        System.out.println(s);
+        System.out.println(str1);
     }
 }
