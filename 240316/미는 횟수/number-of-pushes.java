98c456fc96dbb98d76af37ff9ced6c866498dec8
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-03-16 23:38:46 +0900

    Title: 미는 횟수, Time: 112ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240316/미는 횟수/number-of-pushes.java
@@ -0,0 +1,26 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        String str1 = sc.next();
+        int cnt = 0;
+
+        int len = str1.length();
+
+
+        for (int i = 0; i < str.length(); i++) {
+            if(str1.equals(str.substring(i,len) + str.substring(0,i))){
+                break;
+            }
+            cnt = i+1;
+
+        }
+
+        System.out.println(cnt);
+
+    }
+}
