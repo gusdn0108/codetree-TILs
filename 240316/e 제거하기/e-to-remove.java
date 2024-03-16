951f701a4fa0f0d8c167a96652bbc7525d10c583
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-03-16 20:29:12 +0900

    Title: e 제거하기, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240316/e 제거하기/e-to-remove.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        int cnt = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == 'e' ){
+                cnt = i;
+                break;
+            }
+        }
+
+        str = str.substring(0,cnt)+str.substring(cnt+1);
+
+        System.out.println(str);
+
+
+    }
+}
