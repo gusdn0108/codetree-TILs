7c6008830f1e12e5fe6ba5b27b4cc2f36b73660c
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-03-17 18:27:33 +0900

    Title: 숫자로 이루어진 사각형, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240317/숫자로 이루어진 사각형/rectangle-with-a-number.java
@@ -0,0 +1,27 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        print5Stars(n);
+
+
+    }
+
+    public static void print5Stars(int n) {
+        int num = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(num+" ");
+                num++;
+                if(num == 10 ){
+                    num = 1;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
