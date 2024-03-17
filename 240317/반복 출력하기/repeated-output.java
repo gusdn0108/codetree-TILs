cc189c0fc86eb4930ff79238767e59764baad731
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-03-17 18:21:59 +0900

    Title: 반복 출력하기, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

--- 240317/반복 출력하기/repeated-output.java
+++ 240317/반복 출력하기/repeated-output.java
@@ -12,8 +12,7 @@ public class Main {
 
     public static void print5Stars(int n) {
         for (int i = 0; i < n; i++) {
-            System.out.print("12345^&*()_");
+            System.out.println("12345^&*()_");
         }
-        System.out.println();
     }
 }
