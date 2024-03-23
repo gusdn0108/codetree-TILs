0b8422e57435425d338e6729b1710850ec990d86
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-03-23 18:44:39 +0900

    Title: 숫자 차례로 출력하기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240323/숫자 차례로 출력하기/to-output-numerically.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static int n;
+
+    public static void printNumber1(int n ){
+        if(n == 0){
+            return;
+        }
+
+        printNumber1(n-1);
+        System.out.print(n+" ");
+
+    }
+
+    public static void printNumber2(int n ){
+        if(n == 0){
+            return;
+        }
+        System.out.print(n+" ");
+        printNumber2(n-1);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        printNumber1(n);
+        System.out.println();
+        printNumber2(n);
+    }
+}
