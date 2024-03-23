9cb82a8e35a0130da94917d3f9e7fb1a545b5678
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-03-23 18:35:46 +0900

    Title: 반복 출력하기 2, Time: 94ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240323/반복 출력하기 2/repeated-output-2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void printHello(int n ){
+        if(n == 0){
+            return;
+        }
+        System.out.println("HelloWorld");
+        printHello(n-1);
+    }
+
+    public static void main(String[] args) {
+        printHello(4);
+    }
+}
