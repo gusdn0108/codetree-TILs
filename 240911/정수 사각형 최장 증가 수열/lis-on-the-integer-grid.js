f85523b691cb9d38936128d342d6fbfa9c9ae472
혀누 <96761523+gusdn0108@users.noreply.github.com>
2024-09-11 23:27:16 +0900

    Title: 정수 사각형 최장 증가 수열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240911/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split("\n");
+const N = Number(input[0]);
+const arr = input.slice(1).map(e => e.split(' ').map(el => Number(el)));
+const dp = Array.from({ length: N }, () => Array.from({ length: N }, () => 1));
+
+const dx = [-1, 0, 1, 0];
+const dy = [0, 1, 0, -1];
+let result = 0;
+for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+        for (let k = 0; k < N; k++) {
+            const nx = i + dx[k];
+            const ny = j + dy[k];
+            if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+            if (arr[nx][ny] < arr[i][j]) {
+                dp[i][j] = Math.max(dp[i][j], dp[nx][ny] + 1);
+                result = Math.max(result, dp[i][j]);
+            }
+        }
+    }
+}
+
+console.log(result);
