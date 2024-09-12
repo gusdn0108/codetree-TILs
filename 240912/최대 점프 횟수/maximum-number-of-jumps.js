ce5d3b6a39da915dd2b88885b3647a950e1bcae0
혀누 <96761523+gusdn0108@users.noreply.github.com>
2024-09-12 23:55:43 +0900

    Title: 최대 점프 횟수, Time: 232ms, Memory: 47MB, Status: Passed - CodeTree

--- 240912/최대 점프 횟수/maximum-number-of-jumps.js
+++ 240912/최대 점프 횟수/maximum-number-of-jumps.js
@@ -6,11 +6,13 @@ const arr = input[1].split(' ').map(Number);
 const dp = new Array(N).fill(0);
 
 for (let i = 0; i < N; i++) {
+    if(arr[i]===0) continue;
     for (let j = 1; j <= arr[i]; j++) {
         if (i + j >= N) continue;
-        if (arr[i + j] === 0) continue;
+        if(i!=0 && dp[i]===0) continue;
         dp[i + j] = Math.max(dp[i + j], dp[i] + 1);
     }
 }
 
+// console.log(dp);
 console.log(Math.max(...dp));
