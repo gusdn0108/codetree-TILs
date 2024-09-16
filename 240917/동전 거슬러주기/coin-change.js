01113e13f05139aa6e4cc53d265adc58b72c08da
혀누 <96761523+gusdn0108@users.noreply.github.com>
2024-09-17 01:09:24 +0900

    Title: 동전 거슬러주기, Time: 171ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240917/동전 거슬러주기/coin-change.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [N, M] = input[0].split(' ').map(Number);
+const coins = input[1].split(' ').map(Number);
+const dp = Array.from({ length: M + 1 }, () => Number.MAX_SAFE_INTEGER);
+
+for (let i = 0; i < N; i++) {
+    dp[coins[i]] = 1;
+}
+
+for (let i = coins[0]; i <= M; i++) {
+    for (let j = 0; j < N; j++) {
+        if(i-coins[j]>0)
+            dp[i] = Math.min(dp[i],dp[i-coins[j]]+1);
+    }
+}
+
+// for(let i = 0; i<M; i++){
+//     console.log(dp[i]);
+// }
+
+
+console.log(dp[M]);
