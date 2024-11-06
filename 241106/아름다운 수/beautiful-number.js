1dc2b8965fa61ed02cf22eb750817062a6b1efca
혀누 <96761523+gusdn0108@users.noreply.github.com>
2024-11-06 23:38:15 +0900

    Title: 아름다운 수, Time: 177ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241106/아름다운 수/beautiful-number.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync(0).toString().trim());
+let answer = 0;
+
+function rec(remain){
+    if(remain === 0){
+        answer++;
+    }
+    for(let i = 1; i <= remain; i++){
+        rec(remain - i);
+    }
+}
+rec(n);
+console.log(answer);
